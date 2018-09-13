805cce673a0313664936e355ddec0a1a39e81bde
Algimantas Skuodis <algimantas.skuodis@visma.com>
2018-09-13 13:06:33 +0300

    Add project files.

---
+++ MonomParse/Monome.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MonomParse
+{
+    internal class Monome
+    {
+        private string expression;
+        private string variable;
+        private int coefficient;
+        private int exponent;
+
+        public Monome(string expression)
+        {
+            this.expression = expression;
+            this.ParseExpression(this.expression);
+        }
+
+        private void ParseExpression(string expression)
+        {
+            this.variable = ParseVariable(expression);
+            this.coefficient = ParseCoefficient(expression);
+            this.exponent = ParseExponent(expression);
+
+        }
+
+        private int ParseExponent(string expression)
+        {
+            int exponent = 1;
+            string stringExponent = "";
+            Match exponentMatch = Regex.Match(expression ?? ""
+                , @"(?<coeficient>\d+)?(?<variable>[A-Za-z]+)?\^?(?<exponent>\d+)?");
+            if (exponentMatch.Success)
+                stringExponent = exponentMatch.Groups["exponent"].Value;
+            int.TryParse(stringExponent, out exponent);
+            return exponent;
+        }
+
+        private int ParseCoefficient(string expression)
+        {
+            int coefficient = 1;
+            string stringCoefficient = "";
+            Match coefficientMatch = Regex.Match(expression ?? "", @"[-+]?\d+");
+            if (coefficientMatch.Success)
+                stringCoefficient = coefficientMatch.Value;
+            int.TryParse(stringCoefficient, out coefficient);
+            return coefficient;
+        }
+
+        private string ParseVariable(string expression)
+        {
+            string result = "";
+            Match variableMatch = Regex.Match(expression ?? "", @"[A-Za-z]+");
+            if (variableMatch.Success)
+                result = variableMatch.Value;
+            return result;
+        }
+
+
+        public string Variable {
+            get {
+                return this.variable;
+            }
+        }
+
+        public int Coefficient {
+            get {
+                return this.coefficient;
+            }
+        }
+
+        public int Exponent
+        {
+            get
+            {
+                return this.exponent;
+            }
+        }
+    }
+}
---
+++ MonomParse/MonomeTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MonomParse
+{
+    [TestFixture]
+    public class MonomeTest
+    {
+        [TestCase(null, "")]
+        [TestCase("", "")]
+        [TestCase("x","x")]
+        [TestCase("XX", "XX")]
+        [TestCase(" 2XX ", "XX")]
+        [TestCase(" -2XX^5 ", "XX")]
+        [TestCase(" -2X X^-5 Y Y ", "X")]
+        [TestCase("-5x^2", "x")]
+        public void ParseMonomVariableResultsIn(string expression, string expectedResult)
+        {
+            Monome monom = new Monome(expression);
+            string result = monom.Variable;
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("5", 5)]
+        [TestCase(" 55 ", 55)]
+        [TestCase(" -55 ", -55)]
+        [TestCase("+1", 1)]
+        [TestCase("-5x^2", -5)]
+        public void ParseMonomCoefficientResultsIn(string expression, int expectedResult)
+        {
+            Monome monom = new Monome(expression);
+            int result = monom.Coefficient;
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("-5x^2", 2)]
+        [TestCase("-5x", 1)]
+        [TestCase("x", 1)]
+        [TestCase("", 1)]
+        public void ParseMonomExponentResultsIn(string expression, int expectedResult)
+        {
+            Monome monom = new Monome(expression);
+            int result = monom.Exponent;
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
